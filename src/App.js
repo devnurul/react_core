3b0473fa1e811155ba0916e4841cc513749740ce
freelancernoor <info@devnurul.me>
2021-09-23 04:06:37 +0600

    my react first day

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Nurul Islam";
+  
+  var style ={
+    backgroundColor: "red"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <p>My first paragraph</p>
+        
+    //   </header>
+    // </div>
+    <h2 style={style}>My Name: {name} </h2>
   );
 }
 
