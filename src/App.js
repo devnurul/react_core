dc8c9ed63bf5bab7df95dd9d4e963d14ffef7151
freelancernoor <info@devnurul.me>
2021-09-24 03:17:57 +0600

    many

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <Component name="Sakib"rank="Number one Alrounder"></Component>
       <Component name="Mahamudullah" rank="Captain" ></Component>
       <Component name="musfikur" rank="Mr. dependable"></Component>
+    </div>,
+    <div>
+      <Product></Product>
     </div>
   );
 }
@@ -24,4 +27,22 @@ function Component (manPower){
   )
 }
 
+
+function Product(props){
+  const productStyle={
+    border:" 1px solid gray",
+    borderRedius: "5px",
+    backgorundColor: "lightgray",
+    height:"200px",
+    width: "200px",
+    float: "left"
+  }
+  return(
+    <div style={productStyle}>
+      <h2>Name:</h2>
+      <h1> </h1>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 export default App;
